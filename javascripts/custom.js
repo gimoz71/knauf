0668cb03f6d1d58efbc22625632a5254e4c38d39
Gianluca Monti <gm3871@gmail.com>
2016-12-14 14:49:54 +0100

    tuning e ottimizzazioni modali

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,5 +1,7 @@
 $('.dropdown-toggle').dropdown();
-
+$('body').on('hidden.bs.modal', '.modal', function () {
+  $(this).removeData('bs.modal');
+});
 var overflowContainer = function (overflowSelector, parentOverflowSelector, differenceSelector, differenceSelectorLast) {
     return $(overflowSelector).css("max-height", ($(parentOverflowSelector).height() - $(differenceSelector).outerHeight() - $(differenceSelectorLast).outerHeight()));
 }
