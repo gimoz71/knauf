b4eb2bb06d080a4e15ea6af0c0d98641373f7b32
Gianluca Monti <gm3871@gmail.com>
2016-12-15 14:13:59 +0100

    menu impostazioni verifica

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -2,6 +2,9 @@ $('.dropdown-toggle').dropdown();
 $('body').on('hidden.bs.modal', '.modal', function () {
   $(this).removeData('bs.modal');
 });
+$(document).on('click', '.yamm .dropdown-menu', function(e) {
+  e.stopPropagation()
+})
 var overflowContainer = function (overflowSelector, parentOverflowSelector, differenceSelector, differenceSelectorLast) {
     return $(overflowSelector).css("max-height", ($(parentOverflowSelector).height() - $(differenceSelector).outerHeight() - $(differenceSelectorLast).outerHeight()));
 }
